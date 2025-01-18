65936f74599cb4681b4ba578dd7d7edf09105997
krish <krishnathkkar072004@gmail.com>
2025-01-18 00:30:43 -0500

    Lab Exercose01

--- Lab01_101398578_COMP3133/delete_files.js
+++ Lab01_101398578_COMP3133/delete_files.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
-// File paths
+
 const canadaFile = 'canada.txt';
 const usaFile = 'usa.txt';
 
-// Delete canada.txt if it exists
+
 if (fs.existsSync(canadaFile)) {
   fs.unlinkSync(canadaFile);
   console.log(`${canadaFile} deleted`);
 }
 
-// Delete usa.txt if it exists
 if (fs.existsSync(usaFile)) {
   fs.unlinkSync(usaFile);
   console.log(`${usaFile} deleted`);
